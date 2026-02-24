917b8e53a4cb03fe0988a207e58d7f85073d8235
sergioalcudias-boop <sergioalcudias@gmail.com>
2026-02-23 20:06:36 -0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <h1>HOLA MUNDO</h1>
+        
+        </main>
+    </div>
+  );
+}
